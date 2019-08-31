38140f4508a4a861bdb5edb652c89dd82b0cd5fd
JuhunC <root@localhost.localdomain>
2019-09-01 00:53:48 +0900

    directory update

--- NICER/src/Main/Setup.java
+++ NICER/src/Main/Setup.java
@@ -1,11 +1,13 @@
 package Main;
 
 public class Setup {
-	// Set Default Directories
-	final static String FileSaveDirectory = "/home/joohun484/Desktop/NICER/NICERRESULT/";
-	final static String NICEdir = "/home/joohun484/Desktop/NICER/NICE/";
+	// Change Here
+	final static String mainDir= "/home/user/Desktop/NICER/";
+	
+	final static String FileSaveDirectory = mainDir+"/NICERRESULT/";
+	final static String NICEdir = mainDir+"/NICE/";
 	final static String urlencode = "*";
 	final static String ipAddr = "210.94.194.53:8080/";
 	final static String gceAddr = "104.196.196.160:8080/";
-	final static String PLINKdir = "/home/joohun484/Desktop/NICER/plink";
+	final static String PLINKdir = mainDir+"/plink";
 }
