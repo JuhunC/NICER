684ac76f634a6aef3c93a0dd8502ff2d889a744f
JuhunC <JuhunC>
2019-10-30 15:24:12 +0900

    Update

--- NICER/src/Main/DownloadServlet.java
+++ NICER/src/Main/DownloadServlet.java
@@ -18,7 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Download
  */
-@WebServlet("/DownloadServlet")
+@WebServlet("/Download")
 public class DownloadServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
