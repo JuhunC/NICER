f6afb9c519e11b43f676368aa36224acfa0e9b1f
JungIn-Choi <wd32777@gmail.com>
2020-06-29 20:17:57 +0900

    Update MultiTrans.java

--- NICER/src/Main/MultiTrans.java
+++ NICER/src/Main/MultiTrans.java
@@ -200,7 +200,132 @@ public class MultiTrans {
 		}
 	}
 
-	
+	private void downloadVCF(HttpServletRequest request) {
+		try {
+			Part part = request.getPart("VCFfile");
+			File x = new File(email_dir + "/VCF.vcf");
+			try (InputStream inputStream = part.getInputStream()) {
+				Files.copy(inputStream, x.toPath());
+			}
+			Part part1 = request.getPart("Expfile");
+			File y = new File(email_dir + "/input_y.txt");
+			try (InputStream inputStream = part1.getInputStream()) { // save uploaded file
+				Files.copy(inputStream, y.toPath());
+			}
+
+			Process vcf_data = null;
+			// --vcf test.vcf --allow-extra-chr --no-fid --no-parents --no-sex --no-pheno
+			// --out test
+			vcf_data = Runtime.getRuntime()
+					.exec(Setup.PLINKdir + " --vcf " + email_dir
+							+ "/VCF.vcf --allow-extra-chr --no-fid --no-parents --no-sex --no-pheno --out " + email_dir
+							+ "/input_f");
+			vcf_data.waitFor();
+
+			Process plink_data = null;
+			plink_data = Runtime.getRuntime().exec(Setup.PLINKdir + " --bfile " + email_dir
+					+ "/input_f --allow-extra-chr --recodeA --noweb --maf 0.3 --out " + email_dir + "/input_x");
+			plink_data.waitFor();
+
+			Process pl_to_input = null; // x data
+			pl_to_input = Runtime.getRuntime().exec(Setup.NICEdir + "/pl_to_input " + email_dir);
+			pl_to_input.waitFor();
+
+			Process pl_to_input_y = null; // y data
+			pl_to_input_y = Runtime.getRuntime().exec(Setup.NICEdir + "pl_to_input_y " + email_dir);
+			pl_to_input_y.waitFor();
+
+			// X_rightdim -> X.txt Y_rightdim -> Y.txt
+			transposeFile(email_dir + "/X_rightdim.txt", email_dir + "/X.txt");
+			transposeFile(email_dir + "/Y_rightdim.txt", email_dir + "/Y.txt");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void downloadTransposedXY(HttpServletRequest request) {
+		// Upload SNP file
+		try {
+			Part part = request.getPart("SNPfile2");
+			File x = new File(email_dir + "/X_rightdim.txt");
+			try (InputStream inputStream = part.getInputStream()) { // save uploaded file
+				Files.copy(inputStream, x.toPath());
+			}
+			// Upload Phenotype file
+			Part part1 = request.getPart("Phenotypefile2");
+			File y = new File(email_dir + "/Y_rightdim.txt");
+			try (InputStream inputStream = part1.getInputStream()) { // save uploaded file
+				Files.copy(inputStream, y.toPath());
+			}
+		} catch (Exception e) {
+			printERROR("Error Occured while uploading transposed XY data!!");
+			e.printStackTrace();
+		}
+		transposeFile(email_dir + "X_rightdim.txt", email_dir + "X.txt");
+		transposeFile(email_dir + "Y_rightdim.txt", email_dir + "Y.txt");
+		x_file = new File(email_dir + "/X.txt");
+		y_file = new File(email_dir + "/Y.txt");
+		System.out.println(email_dir);
+	}
+
+	private void downloadXY(HttpServletRequest request) {
+		try {
+			// Upload SNP file
+			Part part = request.getPart("SNPfile");
+			x_file = new File(email_dir + "/X.txt");
+			try (InputStream inputStream = part.getInputStream()) { // save uploaded file
+				Files.copy(inputStream, x_file.toPath());
+			}
+			// Upload Phenotype file
+			Part part1 = request.getPart("Phenotypefile");
+			y_file = new File(email_dir + "/Y.txt");
+			try (InputStream inputStream = part1.getInputStream()) { // save uploaded file
+				Files.copy(inputStream, y_file.toPath());
+			}
+			transposeFile(x_file.getPath(), email_dir + "/Y_rightdim.txt");
+		} catch (Exception e) {
+			printERROR("Error Occurred while uploading XY data!!");
+			e.printStackTrace();
+		}
+	}
+
+	private void downloadPlink(HttpServletRequest request) {
+		try {
+			Part part_00 = request.getPart("SNPfile");
+			Part part_01 = request.getPart("Phenotypefile");
+
+			File file_0 = new File(email_dir + "/input_f.snp");
+			File file_1 = new File(email_dir + "/input_f.phe");
+
+			try (InputStream inputStream = part_00.getInputStream()) { // save uploaded file
+				Files.copy(inputStream, file_0.toPath());
+			}
+			try (InputStream inputStream = part_01.getInputStream()) { // save uploaded file
+				Files.copy(inputStream, file_1.toPath());
+			}
+
+			Process plink_data = null;
+			plink_data = Runtime.getRuntime().exec(Setup.PLINKdir + " --bfile " + email_dir
+					+ "/input_f --recodeA --noweb --maf 0.3 --out " + email_dir + "/input_x");
+			plink_data.waitFor();
+
+			Process pl_to_input = null; // x data
+			pl_to_input = Runtime.getRuntime().exec(Setup.NICEdir + "/pl_to_input " + email_dir);
+			pl_to_input.waitFor();
+
+			Process pl_to_input_y = null; // y data
+			pl_to_input_y = Runtime.getRuntime().exec(Setup.NICEdir + "pl_to_input_y " + email_dir);
+			pl_to_input_y.waitFor();
+
+			// X_rightdim -> X.txt Y_rightdim -> Y.txt
+			transposeFile(email_dir + "/X_rightdim.txt", email_dir + "/X.txt");
+			transposeFile(email_dir + "/Y_rightdim.txt", email_dir + "/Y.txt");
+		} catch (Exception e) {
+			printERROR("Error Occurred while uploading// converting PLINK data!!");
+			e.printStackTrace();
+		}
+	}
+
 	/**
 	 * Error Printing Screen
 	 * 
@@ -221,4 +346,47 @@ public class MultiTrans {
 		return sdfDate.format(now);
 	}
 
+	public static void transposeFile(String inputFile, String outputFile) {
+		try {
+			Vector<String[]> vec = new Vector<>();
+
+			FileReader fr = new FileReader(inputFile);
+			BufferedReader br = new BufferedReader(fr);
+
+			String ln = br.readLine();
+			while (ln != null) {
+				String[] list = ln.split(" ");
+				vec.add(list);
+				ln = br.readLine();
+			}
+			br.close();
+			fr.close();
+
+			int row = vec.size();
+			int col = vec.get(0).length;
+			String mat[][] = new String[row][col];
+			for (int i = 0; i < row; i++) {
+				String[] tmp = vec.get(i);
+				for (int j = 0; j < col; j++) {
+					mat[i][j] = tmp[j];
+				}
+			}
+			vec.clear();
+
+			FileWriter fw = new FileWriter(outputFile);
+			BufferedWriter bw = new BufferedWriter(fw);
+			for (int i = 0; i < col; i++) {
+				for (int j = 0; j < row; j++) {
+					bw.write(mat[j][i] + " ");
+				}
+				bw.write("\n");
+			}
+			bw.close();
+			fw.close();
+
+		} catch (IOException e) {
+			printERROR("Error while transposing " + inputFile + "!!!");
+			e.printStackTrace();
+		}
+	}
 }
